056c54628410acbdcc794d0981de7a25f1da78ef
Dzhamal Abdulbasirov <dzamal26abdulbasirov@gmail.com>
2020-06-21 16:24:12 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Learn more: https://github.com/kennethreitz/setup.py
+import codecs
 
 from setuptools import setup, find_packages
 
@@ -10,9 +11,16 @@ with open('README.rst') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+
+def long_description():
+    with codecs.open('README.rst', encoding='utf8') as f:
+        return f.read()
+
+
 setup(
     name='pyfuncpiper',
-    version='0.1.6',
+    version='0.1.8',
+    long_description=long_description(),
     author='Dzhamal Abdulbasirov',
     author_email='dzamal26abdulbasirov@gmail.com',
     license=license,
